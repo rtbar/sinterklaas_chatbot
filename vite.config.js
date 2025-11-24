cf294920c8f27cfb814ca80929a9ff6c4cc66255
René Barczyk <r.barczyk@mitutoyo.nl>
2025-11-24 16:17:10 +0100

    Setup GitHub Actions for automatic deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/sinterklaas_chatbot/',
+    build: {
+        outDir: 'docs',
+    },
     server: {
         host: true, // Listen on all local IPs
         port: 5173, // Try to stick to 5173
